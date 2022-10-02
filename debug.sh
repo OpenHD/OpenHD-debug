d0cb5218a98fe025147f752bf602fcbfe403f52f
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-02 19:30:13 +0200

    update

--- debug.sh
+++ debug.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "Check if debug file is present"
-if test -f "/conf/boot/debugFile"; then
-cd /conf/boot/
-mkdir debug
-cd debug
-echo "export journalctl"
-echo "Wait 2min until everything is started"
-sleep 2m
-sudo journalctl >> journal.debug
-echo "export service list"
-sudo systemctl list-units --type=service >> services.debug
-echo "export usb-devices"
-sudo lsusb >> usb.debug
-echo "export rtl8812au"
-sudo modinfo 88XXau >> rtl8812au.debug
-sudo iwconfig wlan0 >> rtl8812au.debug
-echo "export dev"
-sudo ls /dev >> dev.debug
-sudo ifconfig >> dev.debug
-
+if test -d "/boot/OpenHD/debug"; then
+    cd /boot/OpenHD/debug
+    echo "export journalctl"
+    echo "Wait 2min until everything is started"
+    sleep 2m
+    sudo journalctl >> journal.debug
+    echo "export service list"
+    sudo systemctl list-units --type=service >> services.debug
+    echo "export usb-devices"
+    sudo lsusb >> usb.debug
+    echo "export video-devices"
+    sudo ls -ltrh /dev/video* >> video.debug
+    echo "export rtl8812au"
+    sudo modinfo 88XXau_wfb >> wifi.debug
+    sudo iwconfig wlan0 >> wifi.debug
+    sudo iwconfig wlan1 >> wifi.debug
+    echo "export dev"
+    sudo ls /dev >> dev.debug
+    sudo ifconfig >> dev.debug
 
 fi
 

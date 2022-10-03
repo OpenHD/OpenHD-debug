d0e99fb93bea82fd9c6d7eb12b3586028b7870b0
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-03 18:13:35 +0200

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 echo "Check if debug file is present"
-if test -d "/boot/OpenHD/debug"; then
+while [test -d "/boot/OpenHD/debug"]
+do
     cd /boot/OpenHD/debug
     echo "export journalctl"
-    echo "Wait 2min until everything is started"
-    sleep 2m
-    sudo journalctl >> journal.debug
+    sudo journalctl > journal.debug
     echo "export service list"
-    sudo systemctl list-units --type=service >> services.debug
+    sudo systemctl list-units --type=service > services.debug
     echo "export usb-devices"
-    sudo lsusb >> usb.debug
+    sudo lsusb > usb.debug
     echo "export video-devices"
-    sudo ls -ltrh /dev/video* >> video.debug
+    sudo ls -ltrh /dev/video* > video.debug
     echo "export rtl8812au"
-    sudo modinfo 88XXau_wfb >> wifi.debug
-    sudo iwconfig wlan0 >> wifi.debug
-    sudo iwconfig wlan1 >> wifi.debug
+    sudo modinfo 88XXau_wfb > wifi.debug
+    sudo iwconfig wlan0 > wlan0.debug
+    sudo iwconfig wlan1 > wlan1.debug
     echo "export dev"
-    sudo ls /dev >> dev.debug
-    sudo ifconfig >> dev.debug
-
-fi
+    sudo ls /dev > dev.debug
+    sudo ifconfig > ifconfig.debug
+    sleep 1m
+done
 

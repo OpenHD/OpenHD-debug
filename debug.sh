b22b90e9cc169fdb12b22ec92b98af0287fed646
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-05 20:58:32 +0200

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Check if debug file is present"
-while test -d "/boot/OpenHD/debug"
+until ! [ -d /boot/OpenHD/debug ]
 do
     cd /boot/OpenHD/debug
     echo "export journalctl"
@@ -13,12 +13,15 @@ do
     echo "export video-devices"
     sudo ls -ltrh /dev/video* > video.debug
     echo "export rtl8812au"
-    sudo modinfo 88XXau_wfb > wifi.debug
+    sudo modinfo 88XXau_wfb > wifi-au.debug
+    sudo modinfo 88XXbu > wifi-bu.debug
     sudo iwconfig wlan0 > wlan0.debug
     sudo iwconfig wlan1 > wlan1.debug
     echo "export dev"
     sudo ls /dev > dev.debug
     sudo ifconfig > ifconfig.debug
-    sleep 1m
+    echo "Wait 2min for the next print everything is started"
+    sleep 2m
 done
+echo "folder is missing"
 
